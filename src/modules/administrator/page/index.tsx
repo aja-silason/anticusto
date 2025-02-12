08fbd700e7059b0e9955fa1850a32483932bbb40
Anania Augusto <ananiasjaimeaugusto@gmail.com>
2025-02-12 10:37:32 +0100

    feat: add user in table

--- src/modules/administrator/page/index.tsx
+++ src/modules/administrator/page/index.tsx
@@ -1,29 +1,6 @@
 import { Barcode, Money, MoneyWavy } from "@phosphor-icons/react";
-import { Container, Card, SalesTable } from "../component";
+import { Container, Card, UserTable } from "../component";
 import { useGetdata } from "../../../common/hook/get/useGetdata";
-
-const mockrowsDay = [
-    { clientName: "Anania Augusto", phoneClient: "944996909", product: "Sofá L - King Size", quantity: 12 },
-    { clientName: "Jaime Augusto", phoneClient: "944996909", product: "Sofá L - King Size", quantity: 12 },
-    { clientName: "Jaime Linice", phoneClient: "944996909", product: "Sofá L - King Size", quantity: 12 },
-  ];
-
-const mockrowsWeek = [
-    { clientName: "Anania Augusto", phoneClient: "944996909", product: "Sofá L - King Size", quantity: 12 },
-    { clientName: "Jaime Augusto", phoneClient: "944996909", product: "Sofá L - King Size", quantity: 12 },
-    { clientName: "Jaime Linice", phoneClient: "944996909", product: "Sofá L - King Size", quantity: 12 },
-    { clientName: "Jaime Linice", phoneClient: "944996909", product: "Sofá L - King Size", quantity: 12 },
-    { clientName: "Jaime Linice", phoneClient: "944996909", product: "Sofá L - King Size", quantity: 12 },
-  ];
-
-const mockrowsMonth = [
-    { clientName: "Anania Augusto", phoneClient: "944996909", product: "Sofá L - King Size", quantity: 12 },
-    { clientName: "Jaime Augusto", phoneClient: "944996909", product: "Sofá L - King Size", quantity: 12 },
-  ];
-
-
-  const allSale = mockrowsDay.concat(mockrowsWeek).concat(mockrowsMonth);
-
 
 export default function HomeAdm() {
 
@@ -38,13 +15,13 @@ export default function HomeAdm() {
                 
                 <Card quantity={accessRole?.length ?? 0} title="Módulos" icon={<MoneyWavy className="w-[2em] h-[2em] text-[#264A7D]" />} />
                 
-                <Card quantity={mockrowsMonth?.length ?? 0} title="Módulos activos" icon={<Money className="w-[2em] h-[2em] text-[#264A7D]" />} />
+                <Card quantity={0} title="Módulos activos" icon={<Money className="w-[2em] h-[2em] text-[#264A7D]" />} />
                 
             </div>
 
             <div className="bg-[#fff] flex gap-[1em] w-[100%] p-[.5em] rounded-[.5em] md:h-[70%]">
 
-                <SalesTable data={allSale} title="Vendas Registradas"/>
+                <UserTable data={user} title="Usuários Registradas"/>
 
             </div>
         </Container>
